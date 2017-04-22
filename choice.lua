6e2d5bbdc1fda694b5e573099b688474cb566cd0
princesspretzel <brittanga@gmail.com>
2017-04-21 23:12:38 -0700

    triangles and conceptual gamework

--- choice.lua
+++ choice.lua
@@ -12,10 +12,23 @@ end
 
 function choiceClass:draw()
     local width, height = love.graphics.getDimensions()
-    love.graphics.print("earth", width/2, height - 100)
-    love.graphics.print("air", width/2, 100)
-    love.graphics.print("fire", 100, height/2)
-    love.graphics.print("water", width - 100, height/2)
+
+    love.graphics.polygon('fill', 0, 0, width, 0, width/2, height/2)
+    love.graphics.setColor(127, 0, 255, 255)
+    love.graphics.polygon('fill', 0, 0, 0, height, width/2, height/2)
+    love.graphics.setColor(255, 255, 102, 255)
+    love.graphics.polygon('fill', 0, height, width, height, width/2, height/2)
+    love.graphics.setColor(255, 204, 204, 255)
+    love.graphics.polygon('fill', width, 0, width, height, width/2, height/2)
+
+    love.graphics.setColor(124,252,0, 255)
+    love.graphics.print("enlightenment", width/2 - 50, height - 100, 0, 1, 1)
+    love.graphics.setColor(204, 204, 255, 255)
+    love.graphics.print("relief", width/2 - 20, 100, 0, 1, 1)
+    love.graphics.setColor(251, 51, 51, 255)
+    love.graphics.print("love", 100, height/2, 0, 1, 1)
+    love.graphics.setColor(0, 191, 255, 255)
+    love.graphics.print("achievement", width - 100 - 50, height/2, 0, 1, 1)
 end
 
 function choiceClass:update(dt)
