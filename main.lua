7816b78bb5af80463937edaca50c86819689c1cd
princesspretzel <brittanga@gmail.com>
2017-04-21 21:40:37 -0700

    collision

--- main.lua
+++ main.lua
@@ -2,7 +2,28 @@ local entities = {}
 local Player = require('player')
 local touching = false
 
+local level = Level
+local player = Player(400, 400)
+local window = Window
 table.insert(entities, player)
+
+-- is a inside b?
+function isInside(a, b)
+    local inside = true
+    if a.x <= b.x then
+        inside = false
+    end
+    if (a.x + a.w) >= (b.x + b.w) then
+        inside = false
+    end
+    if a.y <= b.y then
+        inside = false
+    end
+    if (a.y + a.h) >= (b.y + b.h) then
+        inside = false
+    end
+    return inside
+end
 
 function love.draw()
     love.window.setTitle('you are your own worst enemy')
