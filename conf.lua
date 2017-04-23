883700ccd79bf323e56682def5a217eb5d12f752
princesspretzel <brittanga@gmail.com>
2017-04-23 14:30:10 -0700

    level scaling up

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 love.conf = function(config)
-    config.window.width = 600
-    config.window.height = 600
+    config.window.width = 800
+    config.window.height = 800
     config.author = "Brittany Wald"
 end
--- level.lua
+++ level.lua
@@ -15,28 +15,35 @@ end
 
 function levelClass:update(dt)
     w, h = love.graphics.getDimensions()
-    if w < 600 or h < 600 then
+    if w < 700 or h < 700 then
         if not (chosen == '') then
             self.title = 'excellent decision'
         else
             self.title = 'prepare for the takeover of your small small world'
+        end        
+    end
+    if w < 550 or h < 550 then
+        if not (chosen == '') then
+            self.title = 'don\'t make a big deal out of something so tiny'
+        else
+            self.title = 'is it getting stuffy in here or wut'
         end
     end
-    if w < 500 or h < 500 then
+    if w < 400 or h < 400 then
         if not (chosen == '') then
             self.title = 'indecision or laziness?'
         else
             self.title = 'it\'s coming'
         end
     end    
-    if w < 400 or h < 400 then
+    if w < 250 or h < 250 then
         if not (chosen == '') then
             self.title = 'it\'s here'
         else
             self.title = 'some choices are made for us'
         end
     end
-    if w < 300 or h < 300 then
+    if w < 100 or h < 100 then
         self.title = 'it\'s all there is'
     end
 end
